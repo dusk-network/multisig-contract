f4cb2f3c914b46de817c7948b85002ef6ab50f18
Eduardo Leegwater Simões <eduardo@dusk.network>
2024-10-29 00:04:24 +0100

    Test querying the contract state

--- tests/src/main.rs
+++ tests/src/main.rs
@@ -1,7 +1,10 @@
+use std::sync::mpsc;
+
 use execution_core::{ContractId, StandardBufSerializer};
 use rusk_abi::{CallReceipt, ContractData, PiecrustError, Session};
 
 use bytecheck::CheckBytes;
+use rkyv::de::deserializers::SharedDeserializeMap;
 use rkyv::validation::validators::DefaultValidator;
 use rkyv::{Archive, Deserialize, Infallible, Serialize};
 
@@ -70,11 +73,59 @@ impl ContractSession {
         self.session.call(CONTRACT_ID, fn_name, fn_arg, u64::MAX)
     }
 
+    fn feeder_query<A, R>(&mut self, fn_name: &str, fn_arg: &A) -> Result<Vec<R>>
+    where
+        A: for<'b> Serialize<StandardBufSerializer<'b>>,
+        A::Archived: for<'b> CheckBytes<DefaultValidator<'b>>,
+        R: Archive,
+        R::Archived: Deserialize<R, SharedDeserializeMap>
+            + Deserialize<R, Infallible>
+            + for<'b> CheckBytes<DefaultValidator<'b>>,
+    {
+        let mut results = Vec::new();
+        let (sender, receiver) = mpsc::channel();
+        self.session
+            .feeder_call::<_, ()>(CONTRACT_ID, fn_name, fn_arg, u64::MAX, sender)?;
+
+        for bytes in receiver.into_iter() {
+            results.push(rkyv::from_bytes(&bytes).map_err(|_| PiecrustError::MissingFeed)?);
+        }
+
+        Ok(results)
+    }
+
     fn create_account(&mut self) -> u128 {
         let pks = self.pks.clone();
-        self.call("create_account", &pks)
+        let id = self
+            .call("create_account", &pks)
+            .expect("Creating an account should succeed")
+            .data;
+        self.account_id = Some(id);
+        id
+    }
+
+    fn account(&mut self) -> AccountData {
+        let id = self
+            .account_id
+            .expect("must call `create_account` before `account`");
+        self.call("account", &id)
             .expect("Creating an account should succeed")
             .data
+    }
+
+    fn account_keys(&mut self) -> Vec<PublicKey> {
+        let id = self
+            .account_id
+            .expect("must call `create_account` before `account_keys`");
+
+        self.feeder_query("account_keys", &id)
+            .expect("Feeding account keys should succeed")
+    }
+
+    fn key_accounts(&mut self, index: usize) -> Vec<u128> {
+        let key = self.pks[index].clone();
+        self.feeder_query("key_accounts", &key)
+            .expect("Feeding key accounts should succeed")
     }
 }
 
@@ -82,7 +133,26 @@ impl ContractSession {
 fn create_account() {
     let mut rng = StdRng::seed_from_u64(0xBEEF);
     let mut session = ContractSession::new(&mut rng, 32);
+
     session.create_account();
+
+    assert_eq!(
+        session.account_keys(),
+        session.pks,
+        "Account keys should be the ones used in creating it"
+    );
+
+    let id = session.account_id.unwrap();
+
+    for (i, _) in session.pks.clone().into_iter().enumerate() {
+        let ids = session.key_accounts(i);
+        assert_eq!(
+            ids.len(),
+            1,
+            "The public key should only be used by one account"
+        );
+        assert_eq!(ids[0], id, "The ID should be of the created account");
+    }
 }
 
 fn main() {
